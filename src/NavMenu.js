d0563d269d741a10a9e40e9e1c973dbce3829528
Emilee <emmeng@umich.edu>
2023-06-06 13:50:37 -0400

    tiny addition

---
+++ src/NavMenu.js
@@ -0,0 +1,15 @@
+const NavMenu = () => {
+    return (
+        <nav className="navMenu">
+            <div className="navLinks"> 
+            < a href = "/"> Home </a>
+            < a href="/"> About </a>
+            < a href="/"> Presskit </a>
+            < a href="/"> FAQ </a>
+            < a href="/"> Contact </a>
+            </div>
+        </nav>
+    );
+}
+
+export default NavMenu
